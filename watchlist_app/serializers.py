f7cc173a3967814551cf28faccdd179138a58ec3
rana-093 <masudrana201505093@gmail.com>
2021-11-15 22:20:31 +0600

    Added Validators

--- watchlist_app/serializers.py
+++ watchlist_app/serializers.py
@@ -1,12 +1,17 @@
 from django.utils.translation import activate
 from watchlist_app.models import Movie
-from rest_framework import serializers
+from rest_framework import serializers, validators
+
+
+def description_length(value):
+    if len(value) < 5:
+        raise serializers.ValidationError("Desc. is too short")
 
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[description_length])
     active = serializers.BooleanField()
 
     def create(self, validated_data):
@@ -19,3 +24,13 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    def validate(self, data):  # Validate Object
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Should not be same')
+        return data
+
+    def validate_name(self, value):  # Validate Field
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short")
+        return value
